708986e88d83d198ffd2321557088cf2bace6bdb
Su Myat <myats172@gmail.com>
2020-03-20 14:01:56 +0630

    Changed Phase

---
+++ app.js
@@ -0,0 +1,22 @@
+var p = document.getElementsByTagName('p');
+
+for(let pColor of p){
+	pColor.style.borderColor='#9e077c';
+}
+
+p[0].style.top = '0px';
+p[0].style.left = '585px';
+
+p[1].style.top = '130px';
+p[1].style.left = '405px';
+
+p[2].style.top = '255px';
+p[2].style.left = '233px';
+
+p[3].style.top = '385px';
+p[3].style.left = '54px';
+
+p[4].style.top = '510px';
+p[4].style.right = '130px';
+p[4].style.letterSpacing = '1px'
+p[4].style.padding = '3px 5px'
