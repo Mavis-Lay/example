143e8f26db2345e872fcce3576515b47d66722aa
Su Myat <myats172@gmail.com>
2020-03-20 14:31:46 +0630

    Songs Added

--- app.js
+++ app.js
@@ -8,15 +8,15 @@ p[0].style.top = '0px';
 p[0].style.left = '585px';
 
 p[1].style.top = '130px';
-p[1].style.left = '405px';
+p[1].style.left = '345px';
 
 p[2].style.top = '255px';
-p[2].style.left = '233px';
+p[2].style.left = '40px';
 
 p[3].style.top = '385px';
-p[3].style.left = '54px';
+p[3].style.right = '270px';
 
-p[4].style.top = '510px';
-p[4].style.right = '130px';
+p[4].style.top = '390px';
+p[4].style.left = '480px';
 p[4].style.letterSpacing = '1px'
 p[4].style.padding = '3px 5px'
